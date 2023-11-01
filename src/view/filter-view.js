6c5f8260b772d503b15fc1645e4faba904c03413
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-11-01 23:08:16 +0300

    add filter view

---
+++ src/view/filter-view.js
@@ -0,0 +1,36 @@
+import {createElement} from '../render.js';
+
+// function createNewFilterItemTemplate() {
+//   return '<div class="trip-filters__filter"><input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked><label class="trip-filters__filter-label" for="filter-everything">Everything</label></div>';
+// }
+
+// function createNewEmptyFilterTemplate() {
+//   return '<form class="trip-filters" action="#" method="get"><button class="visually-hidden" type="submit">Accept filter</button></form>';
+// }
+
+function createNewFilterTemplate() {
+  return '<form class="trip-filters" action="#" method="get"><div class="trip-filters__filter"><input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked><label class="trip-filters__filter-label" for="filter-everything">Everything</label></div><div class="trip-filters__filter"><input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future"><label class="trip-filters__filter-label" for="filter-future">Future</label></div><div class="trip-filters__filter"><input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present"><label class="trip-filters__filter-label" for="filter-present">Present</label></div><div class="trip-filters__filter"><input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past"><label class="trip-filters__filter-label" for="filter-past">Past</label></div><button class="visually-hidden" type="submit">Accept filter</button></form>';
+}
+
+class FilterView {
+
+  // amountFilterItem = 4;
+
+  getTemplate() {
+    return createNewFilterTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
+export {FilterView};
