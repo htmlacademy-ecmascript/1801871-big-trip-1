222dbe87eccfe6e61370c88d3479aed0a8bfe42c
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-11-03 11:51:46 +0300

    add view
    Add trip-point-views(new, edit, normal).
    Change template in view with ``.

--- src/main.js
+++ src/main.js
@@ -2,17 +2,23 @@ import { render } from './render.js';
 import { FilterView } from './view/filter-view.js';
 import { TripInfoView } from './view/trip-info-view.js';
 import { SortView } from './view/sort-view.js';
-import { TripPointsView } from './view/trip-points-view.js';
+import { TripPointView } from './view/trip-point-view.js';
+import { TripPointEditView } from './view/trip-point-edit-view.js';
+import { TripPointNewView } from './view/trip-point-new-view.js';
 
 const siteBodyElement = document.querySelector('.page-body');
 const tripHeaderMainElement = siteBodyElement.querySelector('.trip-main');
 const tripControlsfilterElement = siteBodyElement.querySelector('.trip-controls__filters');
+const tripEventsListElement = siteBodyElement.querySelector('.trip-events__list');
 const tripEventsElement = siteBodyElement.querySelector('.trip-events');
 
 render(new FilterView(), tripControlsfilterElement);
 render(new TripInfoView(), tripHeaderMainElement, 'afterbegin');
-render(new SortView(), tripEventsElement);
-render(new TripPointsView(), tripEventsElement);
-render(new TripPointsView(), tripEventsElement);
-render(new TripPointsView(), tripEventsElement);
+render(new SortView(), tripEventsElement, 'afterbegin');
+render(new TripPointEditView(), tripEventsListElement);
+render(new TripPointNewView(), tripEventsListElement);
+render(new TripPointView(), tripEventsListElement);
+render(new TripPointView(), tripEventsListElement);
+render(new TripPointView(), tripEventsListElement);
+
 
